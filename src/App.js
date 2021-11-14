9e2a60f6aa35d2675f8c907b3d27617bb4684cac
Xavier Chum <alejandro.chum@gmail.com>
2021-11-14 12:37:51 -0500

    Debits Page created and linked to home page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import LogIn from './components/Login';
+import Debits from './components/Debits';
 
 class App extends Component {
 
@@ -59,13 +60,14 @@ class App extends Component {
       <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
     );
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
-
+    const DebitsComponent = () => (<Debits />)
     return (
       <Router>
         <div>
           <Route exact path="/" render={HomeComponent} />
           <Route exact path="/userProfile" render={UserProfileComponent} />
           <Route exact path="/login" render={LogInComponent} />
+          <Route exact path="/debits" render={DebitsComponent} />
         </div>
       </Router>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import AccountBalance from './AccountBalance';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
-        <div>
-          <img src="https://picsum.photos/201" alt="bank"/>
-          <h1>Bank of React</h1>
+      <div>
+        <img src="https://picsum.photos/201" alt="bank" />
+        <h1>Bank of React</h1>
 
-          <Link to="/userProfile">User Profile</Link>
+        <div><Link to="/userProfile">User Profile</Link></div>
+        <div><Link to="/debits"> Debits </Link></div>
+        <div><Link to="/credits"> Credits </Link></div>
 
-          <AccountBalance accountBalance={this.props.accountBalance}/>
-        </div>
+        <AccountBalance accountBalance={this.props.accountBalance} />
+      </div>
     );
   }
 }
